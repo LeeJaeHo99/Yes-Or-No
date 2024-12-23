9bfc14045e476441655d5489cd61f7a1e99364f0
LeeJaeHo99 <ljh2735294@naver.com>
2024-12-23 23:06:09 +0900

    Speech Bubble 컴포넌트 생성

--- src/components/Boy.tsx
+++ src/components/Boy.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import SpeechBubble from './SpeechBubble';
 
 export default function Boy() {
     return (
         <div className="flex items-center justify-center flex-col w-full mt-10">
-            <div className="relative flex items-center justify-center w-full">
+            {/* <div className="relative flex items-center justify-center w-full">
                 <span className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-90%] font-DGM text-3xl text-gray-500 text-center">내가 한번 평가해보지</span>
                 <Image
                     style={{width: '60%'}}
@@ -12,7 +13,8 @@ export default function Boy() {
                     height={320}
                     alt="말풍선"
                 />
-            </div>
+            </div> */}
+            <SpeechBubble/>
             <Image
                 src={"/images/boy.png"}
                 width={240}
---
+++ src/components/SpeechBubble.tsx
@@ -0,0 +1,19 @@
+export default function SpeechBubble() {
+    return (
+        <div id="speech-bubble">
+            <div id="bub-part-a"></div>
+            <div id="bub-part-b"></div>
+            <div id="bub-part-c"></div>
+            <div id="speech-txt">Hello!</div>
+            <div id="bub-part-c"></div>
+            <div id="bub-part-b"></div>
+            <div id="bub-part-a"></div>
+            <div id="speech-arrow">
+                <div id="arrow-w"></div>
+                <div id="arrow-x"></div>
+                <div id="arrow-y"></div>
+                <div id="arrow-z"></div>
+            </div>
+        </div>
+    );
+}
