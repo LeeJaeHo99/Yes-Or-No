450032715be335b69d5910b95b136cdde409c89c
LeeJaeHo99 <ljh2735294@naver.com>
2024-12-24 00:02:59 +0900

    Loading 컴포넌트 UI 수정

--- src/app/coin/page.tsx
+++ src/app/coin/page.tsx
@@ -1,16 +1,16 @@
 import Inner from "@/components/Inner";
-import PixelBoxAndInput from '@/components/PixelBoxAndInput';
-import Boy from '@/components/Boy';
-import Loading from '../../components/Loading';
+import PixelBoxAndInput from "@/components/PixelBoxAndInput";
+import Boy from "@/components/Boy";
+import Loading from "../../components/Loading";
 
 export default function CoinPage() {
-
-
     return (
-        <Inner>
-            <Loading/>
-            <PixelBoxAndInput name={'코인 종목'}/>
-            <Boy/>
-        </Inner>
+        <>
+            <Loading />
+            <Inner>
+                <PixelBoxAndInput name={"코인 종목"} />
+                <Boy />
+            </Inner>
+        </>
     );
 }
