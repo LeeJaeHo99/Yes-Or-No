d015a3bcb5ae13389ac72bf61200737491749de4
LeeJaeHo99 <ljh2735294@naver.com>
2024-12-23 23:14:06 +0900

    Speech Bubble 컴포넌트 UI 수정

--- src/components/SpeechBubble.tsx
+++ src/components/SpeechBubble.tsx
@@ -9,6 +9,7 @@ export default function SpeechBubble() {
             <div id="bub-part-b"></div>
             <div id="bub-part-a"></div>
             <div id="speech-arrow">
+                <div className="absolute left-1/2 bottom-[14px] translate-x-[-50%] w-3 h-[2px] bg-white"></div>
                 <div id="arrow-w"></div>
                 <div id="arrow-x"></div>
                 <div id="arrow-y"></div>
