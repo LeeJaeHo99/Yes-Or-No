7154e418df83bb865d11fd9044eeee4bc9c07b27
LeeJaeHo99 <ljh2735294@naver.com>
2024-12-28 09:29:01 +0900

    다시하기 버튼 컴포넌트 생성

---
+++ src/components/Restart.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+export default function Restart({result}){
+    const refreshHandler = () => {
+        window.location.reload();
+    }
+
+    return(
+        <div className='flex flex-col items-center cursor-pointer' onClick={refreshHandler}>
+            <Image
+                src={`/images/${result ? 'true' : 'false'}-graph.png`}
+                width={60}
+                height={60}
+                alt='그래프 이미지'
+            />
+            <span className='font-DGM'>다시 하기</span>
+        </div>
+    );
+}
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,19 +1,22 @@
 "use client";
 import { useSelector } from "react-redux";
+import Restart from './Restart';
 
 export default function Result() {
     const inputResult = useSelector((state) => state.inputResult);
     const inputTextNum = inputResult.length;
-    console.log('inputTextNum: ', inputTextNum);
     let result = useSelector(state => state.result);
     result = inputTextNum % 2 === 0 ? true : false;
 
     return (
-        <div className="font-DGM text-3xl">
-            <span className="font-extrabold">{inputResult}</span> is 
-            <span className="font-extrabold text-4xl text-blue-400" style={{ color: result ? '#f87171' : '#60a5fa' }}>
-                {result ? ` 떡상` : ' 떡락'}
-            </span> 할 운명입니다.
-        </div>
+        <>
+            <div className="mb-6 font-DGM text-3xl">
+                <span className="font-extrabold">{inputResult}</span> is 
+                <span className="font-extrabold text-4xl text-blue-400" style={{ color: result ? '#f87171' : '#60a5fa' }}>
+                    {result ? ` 떡상` : ' 떡락'}
+                </span> 할 운명입니다.
+            </div>
+            <Restart result={result}/>
+        </>
     );
 }
