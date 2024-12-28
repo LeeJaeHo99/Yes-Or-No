6ee170792ca125716d3e183eaf44f562ed7378cc
LeeJaeHo99 <ljh2735294@naver.com>
2024-12-28 09:29:22 +0900

    주식 페이지 수정

--- src/app/stock/page.tsx
+++ src/app/stock/page.tsx
@@ -1,12 +1,24 @@
+'use client';
+
 import Inner from "@/components/Inner";
 import PixelBoxAndInput from "@/components/PixelBoxAndInput";
-import Boy from '@/components/Boy';
+import Boy from "@/components/Boy";
+import Loading from "@/components/Loading";
+import Result from '@/components/Result';
+import { useSelector } from "react-redux";
 
 export default function StockPage() {
+    const loading = useSelector((state) => state.loading);
+    const clickSubmit = useSelector((state) => state.clickSubmit);
+
     return (
-        <Inner>
-            <PixelBoxAndInput name={"주식 종목"} />
-            <Boy />
-        </Inner>
+        <>
+            {loading && <Loading />}
+            <Inner>
+                {!clickSubmit && <Result/>}
+                {clickSubmit && <PixelBoxAndInput name={"주식 종목"} />}
+                <Boy />
+            </Inner>
+        </>
     );
 }
