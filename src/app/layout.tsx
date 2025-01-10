29670185fcbf0e30a2d9784863e3527875235851
LeeJaeHo99 <ljh2735294@naver.com>
2025-01-10 22:16:22 +0900

    메인 layout.tsx 코드 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,18 @@
+// 📍 GROUP : STYLE
 import "../../public/fonts/fonts.css";
 import "../styles/globals.css";
 import "../styles/media.css";
-import Header from "../components/Header";
-import ReduxProvider from "../store/ReduxProvider";
+
+// 📍 GROUP : COMPONENT
+import Header from "@/components/Header";
+import ReduxProvider from "@/store/ReduxProvider";
 
 export const metadata = {
     title: "극락만이 락이다",
     description:
         "로또 번호 추첨과 자신이 구매 할 코인이나 주식의 미래를 예측하는 웹사이트이다.",
-    keywords: [
-        "로또",
-        "로또번호 추첨",
-        "코인",
-        "비트코인",
-        "주식",
-        "떡상",
-        "떡락",
-    ],
-    icons: {
-        icon: "/favicon.ico",
-    },
+    keywords: ["로또", "로또번호 추첨", "코인", "비트코인", "주식", "떡상", "떡락",],
+    icons: {icon: "/favicon.ico",},
     openGraph: {
         type: "website",
         locale: "ko_KR",
